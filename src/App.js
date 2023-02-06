45d0a3bb55d493daba07db3142deb7f5dab48595
etuong <etuong@gmail.com>
2023-02-05 21:35:53 -0800

    Floating action button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import "./App.scss";
 import Auth from "./components/Auth";
 
+import { Fab, Action } from "react-tiny-fab";
+import "react-tiny-fab/dist/styles.css";
+
+import MenuIcon from "./images/menu.png";
+
 function App() {
-  return <Auth />;
+  return (
+    <>
+      <Auth />
+      <Fab
+        icon={MenuIcon}
+        alwaysShowTitle={true}
+        onClick={() => {}}
+      >
+        <Action text="Email" onClick={() => {}} />
+        <Action text="Help" onClick={() => {}}>
+          <i className="fa fa-help" />
+        </Action>
+      </Fab>
+    </>
+  );
 }
 
 export default App;
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -15,120 +15,104 @@ const Auth = () => {
     prismRef.current.style.transform = "translateZ(-100px) rotateY( -180deg)";
   };
 
-  const showThankYou = () => {
+  const showRegisterConfirmation = () => {
     prismRef.current.style.transform = "translateZ(-100px) rotateX( 90deg)";
   };
 
+  const showForgotPasswordConfirmation = () => {
+    prismRef.current.style.transform = "translateZ(-100px) rotateX( -90deg)";
+  };
+
   return (
-    <div>
-      <div className="wrapper">
-        <div className="rec-prism" ref={prismRef}>
-          <div className="face face-top">
-            <div className="content">
-              <h2>Subscribe</h2>
-              <small>
-                Enter your email so we can send you the latest updates!
-              </small>
-              <form onSubmit={(e) => e.preventDefault()}>
-                <div className="field-wrapper">
-                  <input type="text" name="email" placeholder="email" />
-                  <label>e-mail</label>
-                </div>
-                <div className="field-wrapper">
-                  <input type="submit" onClick={showThankYou} />
-                </div>
-              </form>
+    <div className="wrapper">
+      <div className="rec-prism" ref={prismRef}>
+        <div className="face face-front">
+          <div className="content">
+            <h2>Sign in</h2>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <div className="field-wrapper">
+                <input type="text" name="username" />
+                <label>Username</label>
+              </div>
+              <div className="field-wrapper">
+                <input type="password" name="password" />
+                <label>Password</label>
+              </div>
+              <div className="field-wrapper">
+                <input type="submit" />
+              </div>
+              <span className="psw" onClick={showForgotPassword}>
+                Forgot password?
+              </span>
+              <span className="signup" onClick={showSignup}>
+                Not a user? Sign up
+              </span>
+            </form>
+          </div>
+        </div>
+
+        <div className="face face-back">
+          <div className="content">
+            <h2>Forgot your password?</h2>
+            <small>
+              Enter your email so we can send you a reset link for your password
+            </small>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <div className="field-wrapper">
+                <input type="text" name="email" placeholder="email" />
+                <label>E-mail</label>
+              </div>
+              <div className="field-wrapper">
+                <input type="submit" onClick={showForgotPasswordConfirmation} />
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div className="face face-right">
+          <div className="content">
+            <h2>Sign up</h2>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <div className="field-wrapper">
+                <input type="text" name="email" />
+                <label>E-mail</label>
+              </div>
+              <div className="field-wrapper">
+                <input type="password" name="password" />
+                <label>Password</label>
+              </div>
+              <div className="field-wrapper">
+                <input type="password" name="password2" />
+                <label>Re-enter password</label>
+              </div>
+              <div className="field-wrapper">
+                <input type="submit" onClick={showRegisterConfirmation} />
+              </div>
+              <span className="singin" onClick={showLogin}>
+                Already a user? Sign in
+              </span>
+            </form>
+          </div>
+        </div>
+
+        <div className="face face-top">
+          <div className="content">
+            <div className="confirmation-msg">
+              Your email has been sent <br />
+              <span className="login-again" onClick={showLogin}>
+                Click here to sign in
+              </span>
             </div>
           </div>
+        </div>
 
-          <div className="face face-front">
-            <div className="content">
-              <h2>Sign in</h2>
-              <form onSubmit={(e) => e.preventDefault()}>
-                <div className="field-wrapper">
-                  <input type="text" name="username" placeholder="username" />
-                  <label>username</label>
-                </div>
-                <div className="field-wrapper">
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    autoComplete="new-password"
-                  />
-                  <label>password</label>
-                </div>
-                <div className="field-wrapper">
-                  <input type="submit" onClick={showThankYou} />
-                </div>
-                <span className="psw" onClick={showForgotPassword}>
-                  Forgot Password?{" "}
-                </span>
-                <span className="signup" onClick={showSignup}>
-                  Not a user? Sign up
-                </span>
-              </form>
-            </div>
-          </div>
-
-          <div className="face face-back">
-            <div className="content">
-              <h2>Forgot your password?</h2>
-              <small>
-                Enter your email so we can send you a reset link for your
-                password
-              </small>
-              <form onSubmit={(e) => e.preventDefault()}>
-                <div className="field-wrapper">
-                  <input type="text" name="email" placeholder="email" />
-                  <label>e-mail</label>
-                </div>
-                <div className="field-wrapper">
-                  <input type="submit" onClick={showThankYou} />
-                </div>
-              </form>
-            </div>
-          </div>
-
-          <div className="face face-right">
-            <div className="content">
-              <h2>Sign up</h2>
-              <form onSubmit={(e) => e.preventDefault()}>
-                <div className="field-wrapper">
-                  <input type="text" name="email" placeholder="email" />
-                  <label>e-mail</label>
-                </div>
-                <div className="field-wrapper">
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    autoComplete="new-password"
-                  />
-                  <label>password</label>
-                </div>
-                <div className="field-wrapper">
-                  <input
-                    type="password"
-                    name="password2"
-                    placeholder="password"
-                    autoComplete="new-password"
-                  />
-                  <label>re-enter password</label>
-                </div>
-                <div className="field-wrapper">
-                  <input type="submit" onClick={showThankYou} />
-                </div>
-                <span className="singin" onClick={showLogin}>
-                  Already a user? Sign in
-                </span>
-              </form>
-            </div>
-          </div>
-
-          <div className="face face-bottom">
-            <div className="content">
-              <div className="thank-you-msg">Thank you!</div>
+        <div className="face face-bottom">
+          <div className="content">
+            <div className="confirmation-msg">
+              Thank you for registering. Please check your email. <br />
+              <span className="login-again" onClick={showLogin}>
+                Click here to sign in
+              </span>
             </div>
           </div>
         </div>

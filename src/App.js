3ab25338cf51a9c9417097674bed6b1c0f47a628
etuong <etuong@gmail.com>
2023-06-29 22:04:39 -0700

    New log in form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import "./App.scss";
 import Auth from "./components/Auth";
-
-import { Fab, Action } from "react-tiny-fab";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "react-tiny-fab/dist/styles.css";
 
-import { FaBars } from "react-icons/fa";
 
 function App() {
   return (
-    <>
-      <Auth />
-      {/* <Fab icon={<FaBars />} alwaysShowTitle={true}>
-        <Action text="Email" onClick={() => {}} />
-        <Action text="Help" onClick={() => {}}>
-          <i className="fa fa-help" />
-        </Action>
-      </Fab> */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,10 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
+  const [authMode, setAuthMode] = useState("signin");
 
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin");
+  };
+
+  if (authMode === "signin") {
+    return (
+      <div className="auth-form-container">
+        <form className="auth-form">
+          <div className="auth-form-content">
+            <h3 className="auth-form-title">Sign In</h3>
+            <div className="text-center">
+              Not registered yet?{" "}
+              <span className="link-primary" onClick={changeAuthMode}>
+                Sign Up
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="text-center mt-2">
+              Forgot <a href="#">password?</a>
+            </p>
+          </div>
+          <h4>Login with</h4>
+          <a
+            href="#"
+            class="btn btn-primary bt-social"
+            data-provider="facebook"
+          >
+            Facebook
+          </a>
+          <a href="#" class="btn btn-info bt-social" data-provider="twitter">
+            Twitter
+          </a>
+        </form>
+      </div>
+    );
+  }
 
   return (
-
+    <div className="auth-form-container">
+      <form className="auth-form">
+        <div className="auth-form-content">
+          <h3 className="auth-form-title">Sign In</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
+      </form>
+    </div>
   );
 };
 

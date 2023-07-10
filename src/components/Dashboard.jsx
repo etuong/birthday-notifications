7c50cbb21ac03b6bbac32202dac6b3a634178a4e
etuong <etuong@gmail.com>
2023-07-09 21:34:54 -0700

    Firestore

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,101 +1,8 @@
 import React, { useState } from "react";
-import { logout } from "../services/Firebase";
+import { logout, addCard } from "../services/Firebase";
 import useAuth from "../hooks/useAuth";
 import DatePicker from "react-date-picker";
 import Faq from "./Faq";
-
-const FormModal = (props) => {
-  const [name, setName] = useState(props?.name || "");
-  const [phone, setPhone] = useState(props?.phone || "");
-  const [date, setDate] = useState(props?.date || new Date());
-
-  const handleCreation = (event) => {
-    event.preventDefault();
-    alert(`${name} ${phone} ${date}`);
-  };
-
-  return (
-    <div
-      className="modal fade"
-      id="formModal"
-      data-bs-backdrop="static"
-      data-bs-keyboard="false"
-    >
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Add a New Birthday</h5>
-            <button
-              type="button"
-              className="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="modal-body">
-            <form onSubmit={(e) => handleCreation(e)}>
-              <div className="mb-3">
-                <label htmlFor="name" className="col-form-label">
-                  Person's Name:
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  onChange={(event) => setName(event.target.value)}
-                  placeholder="Jane Doe"
-                  id="name"
-                  required
-                />
-              </div>
-
-              <div className="mb-3">
-                <label htmlFor="phone" className="col-form-label">
-                  Person's Phone Number:
-                </label>
-                <input
-                  type="tel"
-                  className="form-control"
-                  onChange={(event) => setPhone(event.target.value)}
-                  placeholder="911-123-4567"
-                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                  id="phone"
-                  required
-                />
-              </div>
-
-              <div className="mb-3">
-                <label htmlFor="date" className="col-form-label">
-                  Birth Date:
-                </label>
-                <DatePicker
-                  id="date"
-                  onChange={setDate}
-                  clearIcon={null}
-                  openCalendarOnFocus={false}
-                  value={date}
-                  required
-                />
-              </div>
-
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-                <button type="submit" className="btn btn-primary">
-                  Add
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const Dashboard = (props) => {
   const { user } = useAuth();
@@ -104,6 +11,100 @@ const Dashboard = (props) => {
     event.preventDefault();
     const result = await logout();
     props.openToast(result);
+  };
+
+  const FormModal = () => {
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [birthDate, setBirthDate] = useState(new Date());
+
+    const handleCreation = async (event) => {
+      event.preventDefault();
+      const result = await addCard({ name, phone, birthDate });
+      props.openToast(result);
+    };
+
+    return (
+      <div
+        className="modal fade"
+        id="formModal"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Add a New Birthday</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={(e) => handleCreation(e)}>
+                <div className="mb-3">
+                  <label htmlFor="name" className="col-form-label">
+                    Person's Name:
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    onChange={(event) => setName(event.target.value)}
+                    placeholder="Jane Doe"
+                    id="name"
+                    required
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="phone" className="col-form-label">
+                    Person's Phone Number:
+                  </label>
+                  <input
+                    type="tel"
+                    className="form-control"
+                    onChange={(event) => setPhone(event.target.value)}
+                    placeholder="911-123-4567"
+                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    id="phone"
+                    required
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="date" className="col-form-label">
+                    Birth Date:
+                  </label>
+                  <DatePicker
+                    id="date"
+                    onChange={setBirthDate}
+                    clearIcon={null}
+                    openCalendarOnFocus={false}
+                    value={birthDate}
+                    required
+                  />
+                </div>
+
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button type="submit" className="btn btn-primary">
+                    Add
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   };
 
   return (
@@ -176,7 +177,7 @@ const Dashboard = (props) => {
             </div>
             <div className="card-body text-dark">
               <h5 className="card-title">Ethan Uong</h5>
-              <h6 class="card-subtitle mb-2 text-muted">626-951-7753</h6>
+              <h6 className="card-subtitle mb-2 text-muted">626-951-7753</h6>
               <p className="card-text">
                 Ethan will turn 23 years old in 256 days!
               </p>
--- src/services/Firebase.js
+++ src/services/Firebase.js
@@ -1,5 +1,23 @@
 import firebase from "firebase/compat/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+// import {
+//   getFirestore,
+//   collection,
+//   getDocs,
+//   setDoc,
+//   deleteDoc,
+//   doc,
+// } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getFirestore,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+} from "firebase/firestore";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -15,13 +33,6 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 const auth = getAuth();
 export default firebase;
-
-export const getCities = async () => {
-  const citiesCol = collection(db, "cities");
-  const citySnapshot = await getDocs(citiesCol);
-  const cityList = citySnapshot.docs.map((doc) => doc.data());
-  return cityList;
-};
 
 export const registerUser = async (name, email, password) => {
   try {
@@ -88,4 +99,60 @@ export const resetPassword = async (email) => {
     status: "success",
     message: "Please check your email to reset password",
   };
+};
+
+/*
+export const createNote = async (note) => {
+  await setDoc(doc(db, "notes", note.id), note);
+};
+
+export const getNotes = async () => {
+  const notesSnapshot = await getDocs(collection(db, "notes"));
+  const notesList = notesSnapshot.docs.map((doc) => doc.data());
+  return notesList;
+};
+
+export const updateNote = async (note) => {
+  const noteRef = doc(db, "notes", note.id);
+  await updateDoc(noteRef, {
+    description: "New description",
+  });
+};
+
+export const deleteNote = async (note) => {
+  const noteRef = doc(db, "notes", note.id);
+  await deleteDoc(noteRef);
+};*/
+
+export const addCard = async (payload) => {
+  try {
+    await addDoc(collection(db, "jKDgFyaF5KfMoGZ03vzFBx0Kaeq1"), payload);
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
+
+export const deleteCard = async (id, cardId) => {
+  if (cardId) {
+    await deleteDoc(doc(db, id, cardId)).then(() => {
+      return {
+        status: "success",
+        message: "Card has been deleted successfully",
+      };
+    });
+  }
+};
+
+export const getCards = (id, callback) => {
+  return onSnapshot(query(collection(db, id)), (querySnapshot) => {
+    const cards = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    callback(cards);
+  });
 };

95b39b1c60568974714137968aa590bba273901f
etuong <etuong@gmail.com>
2024-12-29 20:45:24 -0800

    Vite refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import "./App.scss";
+import Authentication from "./components/Authentication";
+import Dashboard from "./components/Dashboard";
+import useAuth from "./hooks/useAuth";
 
-function App() {
-  const [count, setCount] = useState(0)
+const toastStyle = {
+  style: {
+    borderRadius: "10px",
+    fontSize: "18px",
+  },
+  duration: 3000,
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const openToast = (result) => {
+  if (result.status === "success") {
+    toast.success(result.message, toastStyle);
+  } else if (result.status === "error") {
+    toast.error(result.message, toastStyle);
+  }
+};
 
-export default App
+const App = () => {
+  const { user } = useAuth();
+
+  return useMemo(() => {
+    return (
+      <>
+        {user ? (
+          <Dashboard openToast={openToast} />
+        ) : (
+          <Authentication openToast={openToast} />
+        )}
+        <Toaster />
+      </>
+    );
+  }, [user]);
+};
+
+export default App;
